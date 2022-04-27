37777bdf0f4f6891e4a4dc833c3cb0c9694a9f5d
Zheyuan Xu <u6964086@anu.edu.au>
2022-04-28 00:32:42 +1000

    not work

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from typing import Set, Tuple
-from typing import List
-from typing import Optional
-from datetime import date
+# from typing import Set, Tuple
+# from typing import List
+# from typing import Optional
+# from datetime import date
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+# from fastapi import FastAPI
+# from pydantic import BaseModel
 
 # session FastAPI
 # app = FastAPI()
+
 
 # class Item(BaseModel):
 #     name: str
 #     price: float
 #     is_offer: Optional[bool] = None
+
 
 # @app.get("/")
 # async def read_root():
@@ -26,7 +28,7 @@ from pydantic import BaseModel
 
 # @app.put("/items/{item_id}")
 # async def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name,"item_price": item.price, "item_id": item_id}
+#     return {"item_name": item.name, "item_price": item.price, "item_id": item_id}
 
 # session Features
 
@@ -55,32 +57,32 @@ from pydantic import BaseModel
 # my_second_user: User = User(**second_user_data)
 
 # Session Python Types Info
-def get_full_name(first_name: str, last_name: str):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
+# def get_full_name(first_name: str, last_name: str):
+#     full_name = first_name.title() + " " + last_name.title()
+#     return full_name
 
 
-print(get_full_name("john", "doe"))
+# print(get_full_name("john", "doe"))
 
 
-def get_name_with_age(name: str, age: int):
-    name_with_age = name + " is this old: " + str(age)
-    return name_with_age
+# def get_name_with_age(name: str, age: int):
+#     name_with_age = name + " is this old: " + str(age)
+#     return name_with_age
 
 
-# def process_items(items: List[str]):
-#     for item in items:
-#         print(item)
+# # def process_items(items: List[str]):
+# #     for item in items:
+# #         print(item)
 
 
-def process_items(items_t: Tuple[int, int, str], items_s: Set[bytes]):
-    return items_t, items_s
+# def process_items(items_t: Tuple[int, int, str], items_s: Set[bytes]):
+#     return items_t, items_s
 
 
-class Person:
-    def __init__(self, name: str):
-        self.name = name
+# class Person:
+#     def __init__(self, name: str):
+#         self.name = name
 
 
-def get_person_name(one_person: Person):
-    return one_person.name
+# def get_person_name(one_person: Person):
+#     return one_person.name
