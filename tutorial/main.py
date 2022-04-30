fa70ed87889c87f05838ac8d98b7316ed17846f2
Zheyuan Xu <u6964086@anu.edu.au>
2022-04-30 21:30:35 +1000

    u

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,7 +1,11 @@
+from typing import Optional
 from fastapi import FastAPI
 from enum import Enum
 
 app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
 
 
 class ModelName(str, Enum):
@@ -10,14 +14,27 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
 
 # Because path operations are evaluated in order, you need to make sure that the path
 # for /users/me is declared before the one for /users/{user_id}
